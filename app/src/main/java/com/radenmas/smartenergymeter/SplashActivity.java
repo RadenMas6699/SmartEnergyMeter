0de3aa6d7c2430da37d8d2be8843c431660bd5e3
RadenMas6699 <radenmas.dev@gmail.com>
2021-08-27 21:58:46 +0800

    Initial commit

--- app/src/main/java/com/radenmas/smartenergymeter/SplashActivity.java
+++ app/src/main/java/com/radenmas/smartenergymeter/SplashActivity.java
@@ -18,6 +18,6 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
-        }, 1200);
+        }, 1500);
     }
 }
